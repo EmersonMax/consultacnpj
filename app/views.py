86a49a2c06a6216db0ef0d8a227e2a2ea1442d97
Emerson <emersonmax19@live.com>
2023-02-18 01:26:48 -0300

    adicionando datatlabes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-
+from .services import  apicnpj
+from .forms import CnpjForm
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == "POST":
+        cnpj = request.POST["cnpj"]
+        estado = request.POST["estado"]
+        context ={
+          'dados':   apicnpj(cnpj,estado),
+          'form_cnpj': CnpjForm(),
+           
+           'consulta': 'yes',
+          
+        }
+        '''if context['dados']['cnpj'] == '':
+            messages.error(request,'Cnpj invalido')
+            return render(request,'cnpj.html',context)
+        else:
+            messages.success(request,'Cnpj Consultado')
+            return render(request,'cnpj.html',context)'''
+    else:
+         context ={
+        
+          'form_cnpj': CnpjForm(),
+
+          'consulta':'no',
+          
+        }
+    return render(request, 'home.html', context)
